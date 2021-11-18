7e4396f78e787dc92b011608495f8521bf4bd128
Alexchou38 <alex38g@gmail.com>
2021-11-18 18:02:58 +0100

    Ajout de la partie kernel pour le install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+#### Kernel ####
+cd linux-5.14.5/
+time make -j 20 
+sudo time make modules_install
+sudo /sbin/installkernel 5.14.5 arch/x86_64/boot/bzImage System.map /boot/
+sudo update-grub
+cd ../
 ### GRUB ###
 
 cd grub/Tela-1080p
