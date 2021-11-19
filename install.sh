53d80633a959ef652cc08cc680610f3e88665ccc
Christopher-Loughran <61974217+Christopher-Loughran@users.noreply.github.com>
2021-11-19 10:18:35 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -15,4 +15,11 @@ sudo ./install.sh
 cd ../..
 
 ### PERSONALISATION ###
+sudo apt install lightdm
+sudo apt-get install numlockx
+sudo systemctl -f enable lightdm
+sudo cp -R lightdm /etc
+sudo cp bg.jpg ~/bg.jpg
+sudo cp kscreenlockerrc ~/.config/kscreenlockerrc
+
 
